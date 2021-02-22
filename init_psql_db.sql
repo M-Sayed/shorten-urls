00081d0021888e0e0ea676334c5af4b8e0c1b0dc
Mahmoud Sayed <mahmoudsayed0155@gmail.com>
2021-02-23 00:04:41 +0200

    Create DB only if not existing

--- init_psql_db.sql
+++ init_psql_db.sql
@@ -1,2 +1,5 @@
-CREATE DATABASE shorty_database_development;
-CREATE DATABASE shorty_database_test;
+SELECT 'CREATE DATABASE shorty_database_development'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shorty_database_development')\gexec
+
+SELECT 'CREATE DATABASE shorty_database_test'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'shorty_database_test')\gexec
